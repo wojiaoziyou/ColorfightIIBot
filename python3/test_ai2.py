2a8febfa90f67446bf698668f4a399558b772288
June <wojiaoziyou@126.com>
2019-10-12 16:53:40 -0700

    build all, upgrade all, then attack

--- python3/test_ai2.py
+++ python3/test_ai2.py
@@ -16,64 +16,77 @@ def play_game(game, room, username, password, join_key):
 
         # ========================= This is the game loop ===============================
         while True:
-            cmd_list = []
-            my_attack_list = []
             if not game.update_turn():
                 break
-    
+
             if game.me == None:
                 continue
     
             me = game.me
 
+        # ================================== policy begins ============================
+            # build all
+            cmd_list = []
+            building = 'home'
+            while building != '':
+                building = ''
+                for cell in me.cells.values():
+                    if cell.owner == me.uid \
+                            and cell.building.is_empty \
+                            and me.gold >= BUILDING_COST[0]:
+                        for pos in cell.position.get_surrounding_cardinals():
+                            c = game.game_map[pos]
+                            # if c.owner == me.uid and (c.building.name == "gold_mine" or c.building.name == "energy_well" or c.building.name == "home"):
+                                # building = BLD_FORTRESS
+                                # building = random.choice([BLD_FORTRESS, BLD_GOLD_MINE, BLD_ENERGY_WELL])
+                            if c.owner == me.uid:
+                                building = random.choice([BLD_GOLD_MINE, BLD_ENERGY_WELL])
+
+                        if building != '':
+                            cmd_list.append(game.build(cell.position, building))
+                            print("We build {} on ({}, {})".format(building, cell.position.x, cell.position.y))
+                            me.gold -= BUILDING_COST[0]
+                game.send_cmd(cmd_list)
+
+            # upgrade all
+            cmd_list = []
+            updatable = True
+            while updatable:
+                updatable = False
+                for cell in me.cells.values():
+                    if cell.building.can_upgrade \
+                            and (cell.building.is_home or cell.building.level < me.tech_level) \
+                            and cell.building.upgrade_gold < me.gold \
+                            and cell.building.upgrade_energy < me.energy:
+                        updatable = True
+                        cmd_list.append(game.upgrade(cell.position))
+                        print("We upgraded ({}, {})".format(cell.position.x, cell.position.y))
+                        me.gold   -= cell.building.upgrade_gold
+                        me.energy -= cell.building.upgrade_energy
+                game.send_cmd(cmd_list)
+
+            # attack
+            cmd_list = []
+            attack_list = []
             for cell in me.cells.values():
-                # attack
                 for pos in cell.position.get_surrounding_cardinals():
                     c = game.game_map[pos]
-                    if c.attack_cost < me.energy and c.owner != game.uid \
-                            and c.position not in my_attack_list \
-                            and len(me.cells) < 75:
+                    if c.attack_cost < me.energy and c.owner != me.uid \
+                            and c.position not in attack_list:
                         cmd_list.append(game.attack(pos, c.attack_cost))
                         print("We are attacking ({}, {}) with {} energy".format(pos.x, pos.y, c.attack_cost))
                         me.energy -= c.attack_cost
-                        my_attack_list.append(c.position)
-    
-                # upgrade
-                if cell.building.can_upgrade \
-                        and (cell.building.is_home or cell.building.level < me.tech_level) \
-                        and cell.building.upgrade_gold < me.gold \
-                        and cell.building.upgrade_energy < me.energy:
-                    cmd_list.append(game.upgrade(cell.position))
-                    print("We upgraded ({}, {})".format(cell.position.x, cell.position.y))
-                    me.gold   -= cell.building.upgrade_gold
-                    me.energy -= cell.building.upgrade_energy
-                    
-                # random build 
-                if cell.owner == me.uid \
-                        and cell.building.is_empty \
-                        and me.gold >= BUILDING_COST[0]:
-                    building = random.choice([BLD_FORTRESS, BLD_GOLD_MINE, BLD_ENERGY_WELL])
-                    cmd_list.append(game.build(cell.position, building))
-                    print("We build {} on ({}, {})".format(building, cell.position.x, cell.position.y))
-                    me.gold -= 100
-    
-            
-            # Send the command list to the server
-            result = game.send_cmd(cmd_list)
-
-
-            # Do this to release all the allocated resources. 
-            if me.gold < 0:
-                game.disconnect()
-        # ==================================== main game loop ends ============================
+                        attack_list.append(c.position)
+            game.send_cmd(cmd_list)
+        # ==================================== policy ends ============================
 
 if __name__ == '__main__':
     # Create a Colorfight Instance. This will be the object that you interact with.
     game = Colorfight()
 
     # ==================== Enter a room ====================================
-    # room = 'public'; join_key = ''
-    room = 'Hell'; join_key = 'Crowley'
+    room = 'public'; join_key = ''
+    # room = 'Hell'; join_key = 'Crowley'
     username = 'DEMON'
     password = 'ineffablehusbands'
 
